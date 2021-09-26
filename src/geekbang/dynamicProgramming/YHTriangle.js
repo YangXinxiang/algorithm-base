9a472573d37879267c325287eecf2700b3b103f5
yangxinxiang <yangxinxiang@100tal.com>
2021-09-26 20:55:01 +0800

    feat: 使用动态规划求最短路径，已经OK，倒推路径部分尚待完善

--- src/geekbang/dynamicProgramming/YHTriangle.js
+++ src/geekbang/dynamicProgramming/YHTriangle.js
@@ -1,8 +1,8 @@
 // 变形的杨辉三角： https://time.geekbang.org/column/article/74788 课后思考
 
  //const triangleArr = [[5],[7,8],[2,3,4],[4,9,6,1],[7,8,9,4,5]];
- //const triangleArr = [[5],[7,8],[2,3,4],[4,9,6,1],[7,8,9,4,5]];
-const triangleArr = [[5],[7,8],[2,3,4]];
+const triangleArr = [[5],[7,8],[2,3,4],[4,9,6,1],[7,8,9,4,5]];
+ //const triangleArr = [[5],[7,8],[2,3,4]];
 let minLen = Infinity;
 let count = 0;
 function findMinn(rowIndex = 0, columnIndex=0,len = triangleArr[0][0], route = [triangleArr[0][0]]){
@@ -97,21 +97,101 @@ function findMinn3(rowIndex=0, columnIndex=0, len=triangleArr[0][0], route=[tria
                 findMinn3(rowIndex+1, columnIndex+1, len + nextRow[columnIndex+1], rRote)
             }
         }
-        
+    }
+}
+
+function createTableArr(rows, columns, defaultValue =0){
+    console.log(`createTableArr :: enter, rows = ${rows}, columns = ${columns}, defaultValue = ${defaultValue}`);
+    const table = [];
+    for(let i=0; i<rows; i++){
+        const rowArr = new Array(columns)
+        rowArr.fill(defaultValue)
+        table.push(rowArr)
+    }
+   // console.log(table);
+    return table;
+}
+
+function findMinnDP(data){
+    const sideCont = data.length;
+    const states =  createTableArr(sideCont, sideCont, 0);
+    states[0][0] = data[0][0];
+    console.log(states)
+    console.log(`~~~~~~~~~~~`)
+    for(let rowIndex = 0; rowIndex< sideCont -1; rowIndex ++ ){
+        const row = data[rowIndex];
+        const nextRowIndex = rowIndex+1;
+        const nextRow = data[nextRowIndex];
+        for(let column=0; column<row.length; column++){
+            const current = states[rowIndex][column];
+            // 看左下角的值
+            const left = current + nextRow[column];
+            // debugger
+            if(states[nextRowIndex][column] === 0){
+                states[nextRowIndex][column] = left
+            }else{
+                if(states[nextRowIndex][column] > left){
+                    states[nextRowIndex][column] = left
+                }
+            }
+            // 看右下角的值
+            const right = current + nextRow[column + 1];
+            if(states[nextRowIndex][column+1] === 0){
+                states[nextRowIndex][column+1] = right
+            }else{
+                if(states[nextRowIndex][column+1] > right){
+                    states[nextRowIndex][column +1] = right
+                }
+            }
+        }
         
     }
-    
-    
+    let min = Math.min(...states[sideCont -1])
+    let minIndex = states[sideCont -1].findIndex(v => v===min);
+    console.log(min, minIndex)
+    // 倒推路径
+    // 开始倒推值
+    const route = [data[sideCont -1][minIndex]];
+    for(let rowIndex = sideCont -1; rowIndex > 0; rowIndex --){
+        let preRow = states[rowIndex -1];
+        
+        let preIndex = 0;
+        if(minIndex ===0){
+            preIndex = 0
+        }else if(minIndex === preRow.length){
+            preIndex = minIndex - 1;
+        }else{
+            // 父节点有两个
+            let pLeft = minIndex-1, pRight = minIndex;
+            if(min === preRow[pLeft]){
+                preIndex = pLeft
+            }else{
+                preIndex = pRight
+            }
+        }
+        min = preRow[preIndex];
+        minIndex = preIndex;
+        route.push(data[rowIndex -1][minIndex]);
+    }
+    route.push(data[0][0])
+
+    console.log(states);
+    console.log(`route = ${route}`)
 }
 
 
 function test1(){
-    findMinn3()
+    findMinn3()    
     console.log(`test1 :: minLen = ${minLen}`)
+}
+
+function  test2(){
+    findMinnDP(triangleArr)
 }
 
 function startTest(){
     test1();
+    test2();
 }
 
 startTest();
