2a1980db09f32f7e09eab6a7ae01fb7eb0cd1ad0
yangxinxiang <yangxinxiang@100tal.com>
2021-09-29 10:05:50 +0800

    feat: 继续完善动态规划找零

--- src/geekbang/dynamicProgramming/giveChange3.js
+++ src/geekbang/dynamicProgramming/giveChange3.js
@@ -13,8 +13,39 @@ const {createTableArr} = require("../../util")
      states[0][restNeed] = 0;
      if(restNeed >= values[0]){
         const currentSheet = Math.floor(restNeed/values[0])
-       // restNeed = 
+        restNeed = restNeed % values[0];
+        states[0][restNeed] = currentSheet; // 状态是记录张数
      }
+
+     for(let rowIndex = 1; rowIndex < values.length; rowIndex++ ){
+        // 如果不用这个面值，直接拷贝上一行
+        for(let columnIndex = 0; columnIndex <= totalNeedGive; columnIndex++){
+          if(states[rowIndex-1][columnIndex] >=0 ){
+            states[rowIndex][columnIndex]  = states[rowIndex-1][columnIndex]; // 如果不用这个面值，直接拷贝上一行
+          }
+        }
+        // 如果用
+        for(let columnIndex = 0; columnIndex <= totalNeedGive; columnIndex++){
+          if(states[rowIndex-1][columnIndex] >=0 ){
+            const currentSheet = Math.floor(columnIndex/values[rowIndex])
+            const currentRest = columnIndex % values[rowIndex];
+            const currentTotal = states[rowIndex-1][columnIndex] + currentSheet
+            const currentState = states[rowIndex][currentRest]
+            if(columnIndex ===0){
+              if(currentState ===-1 || (currentTotal < states[rowIndex][currentRest])){
+                states[rowIndex][currentRest] = currentTotal; // 状态是记录张数
+              } 
+            }else{
+              states[rowIndex][currentRest] = currentTotal; // 状态是记录张数
+            }
+                       
+
+          }
+        }
+        
+     }
+
+     console.log(states)
 
  }
 
